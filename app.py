7ac72bd7feb4ecfe37c882ff9f4f20bb967bc0d6
Lilian Hammache <fayzerdev@gmail.com>
2025-02-07 15:20:23 +0100

    bug app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:301191@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:301191@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
